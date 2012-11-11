7fa104507b2c24eb15e604bc9e9a5baed9da9850
Andrea Angott <aangott@umich.edu>
2012-11-11 15:32:01 -0500

    movie name parsing from right-clicked url working.

--- background.js
+++ background.js
@@ -1,24 +1,21 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Called when a message is passed.  We assume that the content script
-// wants to show the page action.
-function onRequest(msg, sender, sendResponse) { // the params are sent as part of the event that triggers the listener.
+// Called when a message is passed. the params listed are sent 
+// as part of the event that triggers the listener.
+function onRequest(msg, sender, sendResponse) { 
+    // turn on the extension icon in the address bar
+    chrome.pageAction.show(sender.tab.id);
 
-  // turn on the extension icon in the address bar
-  chrome.pageAction.show(sender.tab.id);
-
-  // ensure multiple menus are not created upon multiple calls of this function.
-  chrome.contextMenus.removeAll();  
-  // add the link to the context menu if the user clicks on a link including '&t='
-  chrome.contextMenus.create({'title': 'Try to get trailer', 
-                              'contexts': ['link'],
-                              'targetUrlPatterns': ['*://*/*&t=*'],
-                              'onclick': sendURL});
-  // Return nothing to let the connection be cleaned up.
-  sendResponse({});
-  
+    // this ensures that multiple menus are not created 
+    // upon multiple calls of this function.
+    chrome.contextMenus.removeAll();  
+    // add the link to the context menu if the user 
+    // clicks on a link including '&t=' (i.e., a movie cover)
+    chrome.contextMenus.create({'title': 'Try to get trailer', 
+                                'contexts': ['link'],
+                                'targetUrlPatterns': ['*://*/*&t=*'],
+                                'onclick': sendURL});
+    // Return nothing to let the connection be cleaned up.
+    sendResponse({});
 };
 
 // Listen for the content script to send a message to the background page.
--- trailertimes.js
+++ trailertimes.js
@@ -1,9 +1,22 @@
-/**************************main*******************************/
 if (window.location.host.indexOf('netflix') != -1) {
   // send a message to add the extension's icon to the address bar
   chrome.extension.sendMessage('', ''); 
 }
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    alert('message received: ' + request);
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+    getMovieTitle(message);
   });
+
+
+function getMovieTitle(url) {
+    // find position of '&t=' in url
+    var title = url.substring(url.indexOf('&t=') + 3);
+    title = decodeURIComponent(title);
+    title = title.replace(/\+/g, " ");
+    alert(title);
+
+
+}
+
+
+
