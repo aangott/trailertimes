676eaf218dcb7b09d2774499c6079e49f2d1c840
Andrea Angott <aangott@umich.edu>
2012-11-11 16:39:12 -0500

    added ajax function.

--- trailertimes.js
+++ trailertimes.js
@@ -4,7 +4,7 @@ if (window.location.host.indexOf('netflix') != -1) {
 }
 
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    getMovieTitle(message);
+    getRTId(message);
   });
 
 
@@ -14,9 +14,39 @@ function getMovieTitle(url) {
     title = decodeURIComponent(title);
     title = title.replace(/\+/g, " ");
     alert(title);
-
-
 }
 
+function getRTId(url) {
+    // note that the title will be URI encoded, which
+    // the RT API calls for anyway.
+    var title = url.substring(url.indexOf('&t=') + 3);
+    var requested_url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=';
+    requested_url += title;
+    requested_url += '&page_limit=10&page=1&apikey=de5ugjqstcbpqcsqmcnybuhz';
+    get(requested_url, function(response) {
+        var popup = window.open('', 'Trailer');
+        popup.document.write(response);
+    });
+}
+
+// from 'javascript: the definitive guide', pp.500-501
+function get(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onreadystatechange = function() {
+        // if the request is complete and was successful
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf('xml') !== -1 && request.responseXML) {
+                callback(request.responseXML); 
+            } else if (type === 'application/json') {
+                callback(JSON.parse(request.responseText));
+            } else {
+                callback(request.responseText);
+            }
+        }
+    };
+    request.send(null);    
+}
 
 
